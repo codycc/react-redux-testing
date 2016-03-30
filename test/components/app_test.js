6438e0652b3779542bef123fc32f7a4ff6c86116
Cody Condon <codycondon50@gmail.com>
2016-03-30 13:13:31 -0400

    first test passing desribe  it expect

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,19 @@
-import { renderComponent , expect } from '../test_helper';
+import {renderComponent, expect} from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+//Use 'describe' to group together similar tests
+describe('App', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
+  //Use 'it' to test a single attribute of a target
+  //Only used for report
+  it('shows the correct text', () => {
+
+    // create an instance of app
+    const component = renderComponent(App);
+
+    //Use 'expect' to make an 'assertion' about a target
+    expect(component).to.contain('React simple starter');
+
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
 });
