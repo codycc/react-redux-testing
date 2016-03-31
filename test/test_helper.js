32f67350f99c52029a8c16430459f93392a638ee
Cody Condon <codycondon50@gmail.com>
2016-03-30 21:05:00 -0400

    import reducers createStore and providers

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,14 +1,28 @@
 import jsdom from 'jsdom';
 import jquery from 'jquery';
 import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+import {expect} from 'chai';
+import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers  from '../src/reducers';
 //Set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><html><body></body</html>');
 global.window = global.document.defaultView;
 const $ = jquery(global.window);
 // build 'renderComponent' hlper that should render a given react class
-function renderComponent(ComponentClass) {
-  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+function renderComponent(ComponentClass, props, state) {
+  const componentInstance = TestUtils.renderIntoDocument(
+    <Provider store={createStore(reducers, state)}>
+      <ComponentClass {...props} />
+    </Provider>
+  );
+
+   return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
 }
 //Build helper for simulating events
 
 // Set up chai jquery
+
+export { renderComponent, expect};
