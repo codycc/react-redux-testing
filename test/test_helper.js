c8fd731c5d1f403e2c81c04608046fef5dd2b86c
Cody Condon <codycondon50@gmail.com>
2016-03-30 20:37:55 -0400

    import jsdom and jquery into test suite

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,5 +1,10 @@
-//Set up testing environment to run like a browser in the command line
+import jsdom from 'jsdom';
+import jquery from 'jquery';
 
+//Set up testing environment to run like a browser in the command line
+global.document = jsdom.jsdom('<!doctype html><html><html><body></body</html>');
+global.window = global.document.defaultView;
+const $ = jquery(global.window);
 // build 'renderComponent' hlper that should render a given react class
 
 //Build helper for simulating events
