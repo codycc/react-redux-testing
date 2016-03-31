a4f9742aa12bd9790ba9675eddaee496df7608b0
Cody Condon <codycondon50@gmail.com>
2016-03-30 21:15:54 -0400

    finish setting up test suite

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,28 +1,35 @@
-import jsdom from 'jsdom';
-import jquery from 'jquery';
-import TestUtils from 'react-addons-test-utils';
-import ReactDOM from 'react-dom';
-import {expect} from 'chai';
+import _$ from 'jquery';
 import React from 'react';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducers  from '../src/reducers';
-//Set up testing environment to run like a browser in the command line
-global.document = jsdom.jsdom('<!doctype html><html><html><body></body</html>');
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import jsdom from 'jsdom';
+import chai, { expect } from 'chai';
+import chaiJquery from 'chai-jquery';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
+
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
-const $ = jquery(global.window);
-// build 'renderComponent' hlper that should render a given react class
-function renderComponent(ComponentClass, props, state) {
-  const componentInstance = TestUtils.renderIntoDocument(
+const $ = _$(window);
+
+chaiJquery(chai, chai.util, $);
+
+function renderComponent(ComponentClass, props = {}, state = {}) {
+  const componentInstance =  TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
   );
 
-   return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
+  return $(ReactDOM.findDOMNode(componentInstance));
 }
-//Build helper for simulating events
 
-// Set up chai jquery
+$.fn.simulate = function(eventName, value) {
+  if (value) {
+    this.val(value);
+  }
+  TestUtils.Simulate[eventName](this[0]);
+};
 
-export { renderComponent, expect};
+export {renderComponent, expect};
